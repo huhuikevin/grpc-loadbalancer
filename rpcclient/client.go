e7aa9d7736b722ed4a8cb19e4f48b52367ae2f61
kevin.hu <huhui@sharkgulf.com>
2019-08-26 16:54:16 +0800

    grpc 支持单独的ip:port 访问
    Signed-off-by: kevin.hu <huhui@sharkgulf.com>

--- rpcclient/client.go
+++ rpcclient/client.go
@@ -8,9 +8,19 @@ import (
 func NewGRPCConnction(resolver string, domain string, balance string) (*grpc.ClientConn, error) {
 	//target is for the naming finder,example etcd:///test.example.com
 	//the grpc will use the naming server of "etcd" for name resolver
-	target := resolver + ":///" + domain
+	var target string = ""
+	if resolver != "" {
+		target = resolver + ":///" + domain
+	}else {
+		target = domain
+	}
 	log.Print("target=", target)
-	client, err := grpc.Dial(target, grpc.WithInsecure(), grpc.WithBalancerName(balance))
+	dialOpts := make ([]grpc.DialOption, 0, 0)
+	dialOpts = append(dialOpts, grpc.WithInsecure())
+	if balance != "" {
+		dialOpts = append(dialOpts, grpc.WithBalancerName(balance))
+	}
+	client, err := grpc.Dial(target, dialOpts...)
 	if err != nil {
 		return nil, err
 	}
